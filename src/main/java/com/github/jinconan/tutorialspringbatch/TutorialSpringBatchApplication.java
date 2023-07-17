3ead0cd7338d9afbc181ba90ef62b0158d89ad7e
jin <jinconan@gmail.com>
2023-07-18 01:41:03 +0900

    initial commit

---
+++ src/main/java/com/github/jinconan/tutorialspringbatch/TutorialSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.github.jinconan.tutorialspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialSpringBatchApplication.class, args);
+    }
+
+}
